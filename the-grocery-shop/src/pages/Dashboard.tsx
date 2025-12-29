d2c207748f39132f0943d17c020b9f8c1d6d0f08
omidsdgi <omid69sdgi@gmail.com>
2025-12-29 14:01:51 +0330

    feat: implement initial Dashboard component

---
+++ the-grocery-shop/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import Row from "../Row.tsx";
+import Heading from "../Heading.tsx";
+
+const Dashboard=()=>{
+    return(
+        <Row $type='horizontal'>
+        <Heading as='h1'>Dashboard</Heading>
+        </Row>
+    )
+}
+export default Dashboard;
