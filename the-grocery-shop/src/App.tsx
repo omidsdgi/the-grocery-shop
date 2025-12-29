8ff6fb55a39b7060b365603849a1c34b2e0df306
omidsdgi <omid69sdgi@gmail.com>
2025-12-29 14:03:38 +0330

    feat: setting up initial Routers

--- the-grocery-shop/src/App.tsx
+++ the-grocery-shop/src/App.tsx
@@ -1,9 +1,22 @@
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
+import PageNotFound from "./pages/PageNotFound.tsx";
+import GlobalStyles from "./styles/GlobalStyles.ts";
 
 function App() {
 
-  return (
-   <h1>The grocery shop </h1>
-  )
+    return (
+        <>
+            <GlobalStyles/>
+            <BrowserRouter>
+                <Routes >
+                    <Route index element={<Navigate replace to='dashboard'/> } />
+                    <Route path="dashboard" element={<Dashboard/>} />
+                    <Route path='*' element={<PageNotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
