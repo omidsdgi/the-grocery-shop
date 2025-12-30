805920414ee8b0a8472f745403113f462a63bd87
omidsdgi <omid69sdgi@gmail.com>
2025-12-30 15:44:59 +0330

    feat: apply global style to app

--- the-grocery-shop/src/App.tsx
+++ the-grocery-shop/src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Dashboard from "./pages/Dashboard.tsx";
 import PageNotFound from "./pages/PageNotFound.tsx";
 import GlobalStyles from "./styles/GlobalStyles.ts";
+import AppLayout from "./ui/AppLayout.tsx";
 
 function App() {
 
@@ -10,8 +11,10 @@ function App() {
             <GlobalStyles/>
             <BrowserRouter>
                 <Routes >
+                    <Route element={<AppLayout/>}>
                     <Route index element={<Navigate replace to='dashboard'/> } />
                     <Route path="dashboard" element={<Dashboard/>} />
+                    </Route>
                     <Route path='*' element={<PageNotFound/>}/>
                 </Routes>
             </BrowserRouter>
