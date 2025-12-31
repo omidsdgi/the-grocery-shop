cd0a059d2d87874261d88d2f445ef9b80353904f
omidsdgi <omid69sdgi@gmail.com>
2025-12-31 21:46:01 +0330

    feat: implement initial Settings page

---
+++ the-grocery-shop/src/pages/Settings.tsx
@@ -0,0 +1,11 @@
+import Row from "../Row.tsx";
+import Heading from "../Heading.tsx";
+
+export default function Settings() {
+    return (
+        <Row $type='horizontal'>
+            <Heading as='h1'>Settings</Heading>
+        </Row>
+    );
+}
+
