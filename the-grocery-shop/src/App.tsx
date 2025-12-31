7cee19a17d3f4d6f8d2959c60ff4eae4d781e0a2
omidsdgi <omid69sdgi@gmail.com>
2025-12-31 21:48:40 +0330

    feat(App.tsx):add product, settings, users and menus router

--- the-grocery-shop/src/App.tsx
+++ the-grocery-shop/src/App.tsx
@@ -3,6 +3,10 @@ import Dashboard from "./pages/Dashboard.tsx";
 import PageNotFound from "./pages/PageNotFound.tsx";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 import AppLayout from "./ui/AppLayout.tsx";
+import Product from "./pages/Product.tsx";
+import Settings from "./pages/Settings.tsx";
+import Users from "./pages/Users.tsx";
+import Menus from "./pages/Menus.tsx";
 
 function App() {
 
@@ -14,6 +18,10 @@ function App() {
                     <Route element={<AppLayout/>}>
                     <Route index element={<Navigate replace to='dashboard'/> } />
                     <Route path="dashboard" element={<Dashboard/>} />
+                    <Route path="product" element={<Product/>} />
+                    <Route path="menus" element={<Menus/>} />
+                    <Route path="users" element={<Users/>} />
+                    <Route path="settings" element={<Settings/>} />
                     </Route>
                     <Route path='*' element={<PageNotFound/>}/>
                 </Routes>
