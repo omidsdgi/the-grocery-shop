7c1cfbf32350cec2a687df9fc2db2e75f7fff4bd
omidsdgi <omid69sdgi@gmail.com>
2025-12-31 21:46:35 +0330

    feat: implement initial Users page

---
+++ the-grocery-shop/src/pages/Users.tsx
@@ -0,0 +1,11 @@
+import Row from "../Row.tsx";
+import Heading from "../Heading.tsx";
+
+export default function Users() {
+    return (
+        <Row $type='horizontal'>
+            <Heading as='h1'>Users</Heading>
+        </Row>
+    );
+}
+
