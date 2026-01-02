2c653cd5fdaf4ee7b9e6e105b904202d5241f6a6
omidsdgi <omid69sdgi@gmail.com>
2026-01-02 17:50:37 +0330

    feat(product): log products in console

--- the-grocery-shop/src/pages/Product.tsx
+++ the-grocery-shop/src/pages/Product.tsx
@@ -1,7 +1,14 @@
 import Row from "../Row.tsx";
 import Heading from "../Heading.tsx";
+import {useEffect} from "react";
+import {getProducts} from "../services/apiProducts.ts";
+
 
 export default function Product() {
+
+    useEffect(() => {
+        getProducts().then((product)=>console.log(product));
+    }, []);
     return (
         <Row $type='horizontal'>
             <Heading as='h1'>Product</Heading>
