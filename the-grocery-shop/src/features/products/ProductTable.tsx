c6a592c087b8627e313b276aa26bdc4d9faae247
omidsdgi <omid69sdgi@gmail.com>
2026-01-05 22:34:41 +0330

    feat: implement ProductTable component

--- the-grocery-shop/src/features/products/ProductTable.tsx
+++ the-grocery-shop/src/features/products/ProductTable.tsx
@@ -2,8 +2,10 @@ import styled from "styled-components";
 import {useQuery} from "@tanstack/react-query";
 import {getProducts} from "../../services/apiProducts.ts";
 import Spinner from "../../ui/Spinner.tsx";
+import {ProductRow} from "./ProductRow.tsx";
+import type {ProductType} from "../../type/ProductType.ts";
 
-const StyledTable = styled.div`
+const Table = styled.div`
     border: 1px solid var(--color-grey-200);
 
     font-size: 1.4rem;
@@ -14,14 +16,12 @@ const StyledTable = styled.div`
 
 const TableHeader = styled.header`
     display: grid;
-    grid-template-columns:0.6fr 1.8fr 2.2fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
+    grid-template-columns:auto 1.5fr 1.2fr auto auto auto auto auto auto auto auto auto auto 1fr;
+    column-gap: 1.2rem;
     align-items: center;
-
     background-color: var(--color-grey-50);
     border-bottom: 1px solid var(--color-grey-100);
     text-transform: uppercase;
-    letter-spacing: 0.4px;
     font-weight: 600;
     color: var(--color-grey-600);
     padding: 1.6rem 2.4rem;
@@ -30,17 +30,36 @@ const TableHeader = styled.header`
 export default function ProductTable() {
     const {
         isLoading,
-
+        data: products
     }= useQuery({
         queryKey:["products"],
         queryFn: getProducts
     })
     if (isLoading) return <Spinner />;
+    console.log(products)
     return (
-        <StyledTable>
-            <TableHeader>table of product</TableHeader>
+        <Table role='table'>
+            <TableHeader role='row'>
+              <div>id</div>
+              <div>product</div>
+              <div>category</div>
+              <div>rate</div>
+              <div>weight</div>
+              <div>unit</div>
+              <div>price</div>
+              <div>sale price</div>
+              <div>label</div>
+              <div>description</div>
+              <div>total</div>
+              <div>sold</div>
+              <div>quantity</div>
+              <div>expire date</div>
+            </TableHeader>
+            {products?.map((product:ProductType) => <ProductRow key={product.id} product={product} />)}
 
-        </StyledTable>
+
+
+        </Table>
     );
 }
 
